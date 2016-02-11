b71b01dea25956df6314eb415c7a6f7dfb2da5a7
olenyatko <olga.livchak@gmail.com>
2016-02-11 23:24:36 +0200

    laboratorna1. begin

--- lab1_2.py
+++ lab1_2.py
@@ -7,18 +7,28 @@ class D:
         self.y_k = y_k
 
     def zg(self):
-        self.x_k = [8, 1, 0]
-        self.y_k = [5, 8, 9]
-        self.y_k = self.y_k[::-1]
-        print(self.y_k)
-        print(self.x_k)
-        f_x = ([])
-        for i in range(0, len(self.x_k)):
-            for j in range(0, len(self.y_k)):
-                f_x.append(self.x_k[i]*self.y_k[j-1])
-                print('y_k:', self.y_k[j-1])
-            print('x_k:', self.x_k[i])
-        print(f_x)
+        result = []
+        length = min(len(self.y_k), len(self.x_k))
+        for shift in range(1, 2*len(self.x_k)):
+            f_x = []
+            right_shift = shift
+            left_shift = 0
+            if right_shift > length:
+                left_shift = right_shift - length
+                right_shift = length
+            for i in range(left_shift, right_shift):
+                #print(self.x_k[i],self.y_k[len(self.y_k)- right_shift - left_shift + i])
+                f_x.append(self.x_k[i]*self.y_k[len(self.y_k)- right_shift - left_shift + i])
+            print(f_x)
+            sum = 0
+            for i in f_x:
+                sum += i
+            result.append(sum)
+        return result
 
+    def zg_print(self):
+        print(self.zg())
 
-D(x_k=[], y_k=[]).zg()
+#D(x_k=[6, 4, 2, 7, 9, 3 ], y_k=[4, 9, 0, 8, 1, 7, 2]).zg_print()
+#D(x_k=[8, 1, 0], y_k=[5, 6, 9]).zg_print()
+D(x_k=[6, 4, 7, 0, 8, 1, 2], y_k=[3, 4, 8, 7, 0, 6]).zg_print()
