cf566ff64307599a422200f83d463cde49c38f54
olenatko <masha.livchak@gmail.com>
2016-04-04 23:16:31 +0300

    operations with signals

--- lab2_2.py
+++ lab2_2.py
@@ -28,26 +28,30 @@ class D:
     def reverse(self):
         plt.plot(self.x_k, 'b--')
         plt.plot(self.x_k[::-1])
+        plt.plot(self.y_k, 'ro--')
+        plt.plot(self.y_k[::-1])
         plt.grid()
         plt.title('Реверс по часу')
+        self.x_k[::-1]
         plt.show()
 
     def reverse_print(self):
         print(self.reverse())
 
     def shift(self):
-        a = 2
-        b = -2
-
+        x1 = list(range(0, len(self.x_k)))
+        y1 = list(range(0, len(self.y_k)))
+        t1 = 2
+        t2 = -2
         plt.plot(self.x_k, 'ro--')
         for i in range(0, len(self.x_k)):
-            self.x_k[i] -= a
+            x1[i] -= t1
         plt.plot(self.y_k)
         for i in range(0, len(self.y_k)):
-            self.y_k[i] -= b
+            y1[i] -= t2
 
-        plt.plot(self.x_k, 'ro--')
-        plt.plot(self.y_k)
+        plt.plot(x1, self.x_k, 'ro--')
+        plt.plot(y1, self.y_k)
         plt.grid()
         plt.title('Зсув по часу')
         plt.show()
@@ -56,24 +60,25 @@ class D:
         print(self.shift())
 
     def rozh(self):
-        a = -1
-        b = 1
-        plt.plot(self.x_k, 'ro-')
+        a = 2
+        b = 4
+        x_x = list(range(0, len(self.x_k)))
+        y_x = list(range(0, len(self.y_k)))
+        plt.plot(self.x_k)
         for i in range(0, len(self.x_k)):
-            self.x_k[i] *= a * self.x_k[i]
+            x_x[i] *= a
         plt.plot(self.y_k)
         for i in range(0, len(self.y_k)):
-            self.y_k[i] *= b * self.y_k[i]
+            y_x[i] *= b
 
-        plt.plot(self.x_k, 'ro-')
-        plt.plot(self.y_k)
+        plt.plot(x_x, self.x_k, 'b--')
+        plt.plot(y_x, self.y_k, 'ro--')
         plt.grid()
         plt.title('Розширення')
         plt.show()
 
     def rozh_print(self):
         print(self.rozh())
-
 
     def dodavanna(self):
         print('Додавання')
@@ -89,24 +94,25 @@ class D:
     def mnog_print(self):
         print(self.mnog())
 
+x_k = [7, 4, 8, 0, 1, 3, 2, 7]
+y_k = [8, 7, 7, 5, 8, 1, 0]
 
-
-Object_m = D(x_k=[7, 4, 8, 0, 1, 3, 2, 7], y_k=[8, 7, 7, 5, 8, 1, 0])
+Object_m = D(x_k=x_k, y_k=y_k)
 Object_m.mashtabuvanna_print()
 
-Object_r = D(x_k=[7, 4, 8, 0, 1, 3, 2, 7], y_k=[8, 7, 7, 5, 8, 1, 0])
+Object_r = D(x_k=x_k, y_k=y_k)
 Object_r.reverse_print()
 
-Object_s = D(x_k=[7, 4, 8, 0, 1, 3, 2, 7], y_k=[8, 7, 7, 5, 8, 1, 0])
+Object_s = D(x_k=x_k, y_k=y_k)
 Object_s.shift_print()
 
-Object_rh = D(x_k=[7, 4, 8, 0, 1, 3, 2, 7], y_k=[8, 7, 7, 5, 8, 1, 0])
+Object_rh = D(x_k=x_k, y_k=y_k)
 Object_rh.rozh_print()
 
-Object_add = D(x_k=[7, 4, 8, 0, 1, 3, 2, 7], y_k=[8, 7, 7, 5, 8, 1, 0])
+Object_add = D(x_k=x_k, y_k=y_k)
 Object_add.dodavanna_print()
 
-Object_mn = D(x_k=[7, 4, 8, 0, 1, 3, 2, 7], y_k=[8, 7, 7, 5, 8, 1, 0])
+Object_mn = D(x_k=x_k, y_k=y_k)
 Object_mn.mnog_print()
 
 
